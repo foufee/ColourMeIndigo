8af594840bf4eb134e5a069a631b5a195bd0ae16
Matthew Huck <matthew.huck@gmail.com>
2018-06-02 22:30:54 +1200

    Rotational movement now works

--- src/ducks/filterWheel.js
+++ src/ducks/filterWheel.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import {Map,List} from 'immutable'
 
-const _LEFT = 'filterwheel/LEFT';
-const _RIGHT = 'filterwheel/RIGHT';
-
+const _SELECT_COLOR = 'filterwheel/SELECT_COLOR';
 
 export const InitialState = new Map( {
     selectedColor:0
 })
 
-export function left() {
-    return {type:_LEFT};
-}
-
-export function right() {
-    return {type:_RIGHT}
+export function selectColor(color) {
+    return {type:_SELECT_COLOR, color:color};
 }
 
 export default function reducer(state = InitialState, action) {
     switch(action.type) {
-        case _LEFT: {
-            let c = state.get('selectedColor');
-            if (c == 0) {
-                c = 6
-            } else {
-                c = c-1;
-            }
-            return state.set('selectedColor',  c)
-        }
-        case _RIGHT:
-        {
-            let c = state.get('selectedColor');
-            if (c == 6) {
-                c = 0
-            } else {
-                c = c+1;
-            }
-            return state.set('selectedColor',  c);
+        case _SELECT_COLOR: {
+            return state.set('selectedColor',  action.color)
         }
     }
     return state;
--- src/widgets/filterWheel.js
+++ src/widgets/filterWheel.js
@@ -3,14 +3,14 @@ import {Image,View,Easing,TouchableOpacity,TouchableWithoutFeedback,Text , PanRe
 import {connectWithLifecycle} from "react-lifecycle-component";
 import * as filterWheelActions from "../ducks/filterWheel";
 
-const angle = 360.0/7.0
+const numberOfColors = 7
+const angle = 360.0/numberOfColors
 
 class FilterWheelComponent extends Component {
     rotateWheel = new Animated.Value(0);
 
     state = {
-        start:360-angle,
-        end:360,
+        selectedColor:0
     }
     ;
     constructor(props) {
@@ -20,62 +20,96 @@ class FilterWheelComponent extends Component {
         this._panResponder = PanResponder.create({
             onMoveShouldSetResponderCapture: () => true,
             onMoveShouldSetPanResponderCapture: () => true,
+            onPanResponderGrant: this._handlePanResponderGrant,
             onPanResponderMove: this._handlePanResponderMove,
             onPanResponderRelease:  this._handlePanResponderEnd
         });
     }
 
+    widgetCoordToUnitVector = (v) => {
+        let wx = v[0] - this.state.layout.px;
+        let wy = v[1] - this.state.layout.py;
 
+        let initial_touch_distance_dx = this.state.layout.widget_centerPoint[0] - wx;
+        let initial_touch_distance_dy = this.state.layout.widget_centerPoint[1] - wy;
+
+        let touch_vector_length = Math.sqrt((initial_touch_distance_dx*initial_touch_distance_dx) + (initial_touch_distance_dy*initial_touch_distance_dy));
+
+        return [initial_touch_distance_dx / touch_vector_length, initial_touch_distance_dy / touch_vector_length]
+    }
+
+    angleBetweenPoints = (p1,p2) => {
+        let vp = [p1[0]-p2[0],p1[1]-p2[1]]
+        let angle = 2 * Math.atan2(p2[1]-p1[1], p2[0]-p1[0]);
+        return angle
+
+    }
+
+    _handlePanResponderGrant = (e, gestureState) => {
+        console.log(gestureState,this.state)
+        let sgVp = this.widgetCoordToUnitVector([gestureState.x0,gestureState.y0]);
+        console.log(sgVp)
+        this.setState({
+            startGesture:sgVp
+        })
+        return true;
+    }
+
+    gestureStateToAngle = (gestureState) => {
+        let curLocUV = this.widgetCoordToUnitVector([gestureState.moveX,gestureState.moveY])
+        console.log(curLocUV, this.state.startGesture)
+        let mvAngle = this.angleBetweenPoints(this.state.startGesture, curLocUV);
+        let rotAngle = mvAngle * 180/Math.PI;
+        console.log("RotAngle:",rotAngle)
+        console.log("RotAngleOther",360-rotAngle)
+        return rotAngle;
+    }
 
     _handlePanResponderMove = (e, gestureState) => {
-        console.log("Move")
-        // Just allow a little bit
-        var deg = (Math.atan(gestureState.dx/200)) * 180/Math.PI;
-        var target = deg;
-        console.log(deg, this.state)
-        if (this.state.end == 360 && gestureState.dx > 0) {
-            target = deg;
-        } else if (this.state.start == 0 && gestureState.dx < 0) {
-            target = 360 + angle + deg;
-        } else {
-            target = this.state.end + deg;
-        }
+        console.log(this.state)
+        this.rotateWheel.setValue((this.state.selectedColor * angle) + this.gestureStateToAngle(gestureState));
 
-        console.log("Setting target to:", target)
-        this.rotateWheel.setValue(target);
     }
 
     _handlePanResponderEnd = (e, gestureState) => {
-        const screenWidth = Dimensions.get("window").width;
-
-        if (Math.abs(gestureState.vx) >= 0.5 || Math.abs(gestureState.dx) >= 0.33 * screenWidth) {
-            if (gestureState.dx<0) this.props.onLeft()
-            else this.props.onRight()
-        } else {
-            Animated.spring(this.rotateWheel, {
-                toValue: this.state.end,
-                duration: 1000,
-                bounciness: 10
-            }).start();
+        let deg = Math.abs((this.state.selectedColor * angle) + this.gestureStateToAngle(gestureState));
+        let target = Math.floor(deg / angle);
+        let r = deg % angle;
+        if (r > angle/2.0) {
+            console.log("Jumping")
+            target = target + 1;
         }
+        console.log("Target",target,target%7)
+        this.setState({ selectedColor:target%7 })
+        this.props.onPickColor(target%7)
+        Animated.spring(this.rotateWheel, {
+            toValue: target * angle,
+            duration: 1000,
+            bounciness: 10,
+            useNativeDriver:true
+        }).start();
+    }
 
-
-
+    measureView = (event) => {
+        this.imageComponent.getNode().measure( (fx, fy, width, height, px, py) => {
+            this.setState({
+                layout:{
+                    w:width,
+                    h:height,
+                    fx:fx,
+                    fy:fy,
+                    px:px,
+                    py:py,
+                    widget_centerPoint:[width / 2,height/2]
+                }
+            })
+        })
     }
 
     componentWillReceiveProps(nextProps) {
-
-        console.log("CWRP", nextProps, this.state)
         this.setState( {
-            start:nextProps.start,
-            end:nextProps.end
+            selectedColor:nextProps.selectedColor
         });
-
-        Animated.spring(this.rotateWheel, {
-            toValue: nextProps.end,
-            duration: 1000,
-            bounciness: 10
-        }).start();
     }
 
     render () {
@@ -84,8 +118,8 @@ class FilterWheelComponent extends Component {
             outputRange: ['0deg', '360deg']
         })
         return (
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}} >
-                <Animated.Image  source={require('../../public/filterWheel.png')}
+            <View  style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}} >
+                <Animated.Image ref={component => { this.imageComponent = component ; }} onLayout={this.measureView} source={require('../../public/filterWheel.png')}
                               style={{transform: [{rotate: spin}]}} {...this._panResponder.panHandlers}/>
             </View>
         )
@@ -94,12 +128,8 @@ class FilterWheelComponent extends Component {
 }
 
 const mapStateToProps = (state, ownprops) => {
-    let pc = state.getIn(['filterWheel','previousColor']);
-    let c = state.getIn(['filterWheel','selectedColor']);
-
     var props = {
-        start: (state.getIn(['filterWheel','selectedColor']))*(360/7.0),
-        end: (state.getIn(['filterWheel','selectedColor'])+1)*(360/7.0)
+        selectedColor: (state.getIn(['filterWheel','selectedColor']))
     }
 
     return props;
@@ -107,14 +137,9 @@ const mapStateToProps = (state, ownprops) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onLeft: () => {
-            console.log("Left")
-            dispatch(filterWheelActions.left())
+        onPickColor: (colorIdx) => {
+            dispatch(filterWheelActions.selectColor(colorIdx))
         },
-        onRight: () => {
-            console.log("Right")
-            dispatch(filterWheelActions.right())
-        }
     };
 };
 
